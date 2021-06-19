542b289beb5ce714ad013e8c3c4025cb8f6dee3e
Juan Franco <jfrancoh5@gmail.com>
2021-06-18 20:59:41 -0500

    arreglos al media query cellphone

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const header__burguer_menu = document.querySelector('.header__burguer_menu');
+const header__logo = document.querySelector('.header__logo');
 
+header__logo.addEventListener('click', function(){
+   document.getElementById('header__nav-menu').classList.toggle('active');
+});
 header__burguer_menu.addEventListener('click', function(){
    document.getElementById('header__nav-menu').classList.toggle('active');
-})
+});
